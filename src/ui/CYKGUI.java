d34d5683b8470b3e6c4ef6d03db905b94c743446
CarlosJPantoja <cadavid1230@gmail.com>
2022-05-07 22:07:29 -0500

    Auto-generation of spaces for new non-terminals found in a production

--- src/ui/CYKGUI.java
+++ src/ui/CYKGUI.java
@@ -6,15 +6,16 @@
 */
 package ui;
 
+import java.util.ArrayList;
+
 import com.jfoenix.controls.JFXTextField;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Control;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
-import model.CYK;
 
 public class CYKGUI {
 
@@ -27,30 +28,30 @@ public class CYKGUI {
 
 	@FXML
 	private GridPane gridPane;
-
-	@FXML
-	private JFXTextField productions1;
-
+	/*
 	@FXML
 	private JFXTextField string;
 
 	@FXML
 	private Label result;
+	*/
+	private ArrayList<Control[]> grammar;
 
 	// -----------------------------------------------------------------
 	// Relations
 	// -----------------------------------------------------------------
 
-	private CYK cyk;
+	//private CYK cyk;
 
 	// -----------------------------------------------------------------
 	// Methods
 	// -----------------------------------------------------------------
 
 	public CYKGUI() {
-		cyk = new CYK();
+		grammar = new ArrayList<>();
+		//cyk = new CYK();
 	}
-
+	/*
 	@FXML
 	public void doCYKAlgorithm() {
 		boolean isGenerated = cyk.checkStringGeneration(string.getText());
@@ -59,10 +60,85 @@ public class CYKGUI {
 		else
 			result.setText("False");
 	}
+	*/
+	public void initialize() {
+		Label lb = nonTerminalLabel('S');
+		JFXTextField tf = configureNewTextField();
+		gridPane.add(lb, 0, 0);
+		gridPane.add(tf, 1, 0);
+		Control[] row = new Control[2];
+		row[0] = lb;
+		row[1] = tf;
+		grammar.add(row);
+	}
 
-	@FXML
-	public void verifyProductions(KeyEvent event) {
-		JFXTextField tf = (JFXTextField) event.getSource();
+	private JFXTextField configureNewTextField() {
+		JFXTextField tf = new JFXTextField() {
+			@Override
+			public void cut() {}
+			@Override
+			public void copy() {}
+			@Override
+			public void paste() {}
+		};
+		tf.caretPositionProperty().addListener((observable, oldValue, newValue) -> {
+			if (newValue.intValue() != tf.getText().length()) 
+				tf.positionCaret(tf.getText().length());
+		});		
+		tf.selectedTextProperty().addListener((observable, oldValue, newValue) -> {
+			if (!newValue.isEmpty()) 
+				tf.deselect();
+		});
+		tf.focusedProperty().addListener((observable, oldValue, newValue) -> {
+			if (!newValue && tf.getText()!=null && !tf.getText().equals("")){
+				String[] productions = tf.getText().split("|");
+				for(int i=0; i<productions.length; i++) {
+					char letter = productions[i].charAt(0);
+					if(Character.isUpperCase(letter) && !containsInGrammar(letter)) {
+						Label lb = nonTerminalLabel(productions[i].charAt(0));
+						JFXTextField tfChild = configureNewTextField();
+						gridPane.add(lb, 0, grammar.size());
+						gridPane.add(tfChild, 1, grammar.size());
+						Control[] row = new Control[2];
+						row[0] = lb;
+						row[1] = tfChild;
+						grammar.add(row);
+						mainVBox.getChildren().set(1, gridPane);
+					}
+				}
+			}
+		});
+		tf.setOnKeyTyped(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				verifyProductions(tf, event);
+			}
+		});
+		setStyle(tf);
+		tf.setPrefWidth(200);
+		return tf;
+	}
+	
+	private boolean containsInGrammar(char letter) {
+		for(int i=0; i<grammar.size(); i++) {
+			Label lb = (Label) grammar.get(i)[0];
+			if(letter == lb.getText().charAt(0))
+				return true;
+		}
+		return false;
+	}
+
+	private Label nonTerminalLabel(char letter) {
+		Label lb = new Label(letter+" -> ");
+		setStyle(lb);
+		return lb;
+	}
+
+	private <Type extends Control> void setStyle(Type control) {
+		control.setStyle("-fx-font-family: Consolas; -fx-font-weight: bold; -fx-font-size: 24px");
+	}
+	
+	public void verifyProductions(JFXTextField tf, KeyEvent event) {
 		String txt = tf.getText();
 		int lg = txt.length();
 		String st = event.getCharacter();
@@ -97,18 +173,5 @@ public class CYKGUI {
 		}
 		if (consume)
 			event.consume();
-	}
-
-	public void initialize() {
-		productions1.caretPositionProperty().addListener(new ChangeListener<Number>() {
-
-			@Override
-			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-				if (Integer.parseInt(newValue + "") != productions1.getText().length()) {
-					System.out.println("old: " + oldValue + ", new: " + newValue);
-					productions1.positionCaret(productions1.getText().length());
-				}
-			}
-		});
 	}
 }
